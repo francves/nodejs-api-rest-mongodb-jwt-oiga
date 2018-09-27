f045283b69ae040afac42129bf7ee85d00ca28d3
Francisco Velasquez <francves1711@gmail.com>
2018-09-27 18:18:53 -0400

    Adding orders endpoint

---
+++ controllers/order.js
@@ -0,0 +1,122 @@
+'use strict'
+// We load the models for later use
+const orderModel = require('../models/order');
+
+// Create and Save a new Order
+exports.create = (req, res) => {
+    // Validate request
+    if(!req.body.productId) {
+        return res.status(400).send({
+            message: "Order productId can not be empty"
+        });
+    }
+
+    // Create a Order
+    const order = new orderModel({
+        productId: req.body.productId, 
+        userId: req.body.userId,
+        quantity: req.body.quantity,
+        totalAmount: req.body.totalAmount,
+    });
+
+    // Save Order in the database
+    order.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Order."
+        });
+    });
+};
+
+// Retrieve and return all orders from the database.
+exports.findAll = (req, res) => {
+    orderModel.find()
+    .then(orders => {
+        res.send(orders);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving orders."
+        });
+    });
+};
+
+// Find a single order with a orderId
+exports.findOne = (req, res) => {
+    orderModel.findById(req.params.orderId)
+    .then(order => {
+        if(!order) {
+            return res.status(404).send({
+                message: "Order not found with id " + req.params.orderId
+            });            
+        }
+        res.send(order);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Order not found with id " + req.params.orderId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving order with id " + req.params.orderId
+        });
+    });
+};
+
+// Update a order identified by the orderId in the request
+exports.update = (req, res) => {
+    // Validate Request
+    if(!req.body.productId) {
+        return res.status(400).send({
+            message: "Order productId can not be empty"
+        });
+    }
+
+    // Find order and update it with the request body
+    orderModel.findByIdAndUpdate(req.params.orderId, {
+        productId: req.body.productId, 
+        userId: req.body.userId,
+        quantity: req.body.quantity,
+        totalAmount: req.body.totalAmount,
+    }, {new: true})
+    .then(order => {
+        if(!order) {
+            return res.status(404).send({
+                message: "Order not found with id " + req.params.orderId
+            });
+        }
+        res.send(order);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Order not found with id " + req.params.orderId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating order with id " + req.params.orderId
+        });
+    });
+};
+
+// Delete a order with the specified noteId in the request
+exports.delete = (req, res) => {
+    orderModel.findByIdAndRemove(req.params.orderId)
+    .then(order => {
+        if(!order) {
+            return res.status(404).send({
+                message: "Order not found with id " + req.params.orderId
+            });
+        }
+        res.send({message: "Order deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Order not found with id " + req.params.orderId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete order with id " + req.params.orderId
+        });
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,38 @@
 'use strict'
 
 const express = require('express')
-// We loaded the controller
-const controller = require('../controllers/product');
+// We loaded the controllers
+const controllerProduct = require('../controllers/product');
+const controllerOrders = require('../controllers/order');
 const api = express.Router()
 // Parameters
 const pathProducts = '/products';
 const idProduct = ':productId';
+const pathOrders = '/orders';
+const idOrders = ':orderId';
 
+//PRODUCTS ROUTES
 //Retrieve all products
-api.get(`${pathProducts}`, controller.findAll)
+api.get(`${pathProducts}`, controllerProduct.findAll)
 // Retrieve a single product with productId
-api.get(`${pathProducts}/${idProduct}`, controller.findOne)
+api.get(`${pathProducts}/${idProduct}`, controllerProduct.findOne)
 //Create a new product
-api.post(`${pathProducts}`, controller.create)
+api.post(`${pathProducts}`, controllerProduct.create)
 //Update a product with productId
-api.put(`${pathProducts}/${idProduct}`, controller.update)
+api.put(`${pathProducts}/${idProduct}`, controllerProduct.update)
 // Delete a product with productId
-api.delete(`${pathProducts}/${idProduct}`, controller.delete)
+api.delete(`${pathProducts}/${idProduct}`, controllerProduct.delete)
+
+//ORDERS ROUTES
+//Retrieve all orders
+api.get(`${pathOrders}`, controllerOrders.findAll)
+// Retrieve a single order with orderId
+api.get(`${pathOrders}/${idOrders}`, controllerOrders.findOne)
+//Create a new order
+api.post(`${pathOrders}`, controllerOrders.create)
+//Update a order with orderId
+api.put(`${pathOrders}/${idOrders}`, controllerOrders.update)
+// Delete a order with orderId
+api.delete(`${pathOrders}/${idOrders}`, controllerOrders.delete)
 
 module.exports = api
